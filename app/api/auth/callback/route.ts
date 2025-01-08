90bfe2ceb87fc4ae535e20ce3a880659c8624b5d
Xavier Francisco <xavierfrancisco353@gmail.com>
2025-01-08 12:22:29 +0200

    Fixed dynamic routes

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,3 +1,14 @@
+// import { NextResponse } from 'next/server';
+
+// export async function GET(request: Request) {
+// 	const requestUrl = new URL(request.url);
+// 	const code = requestUrl.searchParams.get('code');
+
+// 	return NextResponse.redirect(requestUrl.origin);
+// }
+
+export const dynamic = 'force-dynamic';
+
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
